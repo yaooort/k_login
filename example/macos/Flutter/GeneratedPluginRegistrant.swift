b571f78c6b9167dc88ea1c4024c4f83ee0c82ea7
oort <yaooort@gmail.com>
2025-03-29 18:22:40 +0800

    del

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,8 @@
 import FlutterMacOS
 import Foundation
 
-import k_login
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  KLoginPlugin.register(with: registry.registrar(forPlugin: "KLoginPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
--- example/windows/flutter/generated_plugins.cmake
+++ example/windows/flutter/generated_plugins.cmake
@@ -3,7 +3,6 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
-  k_login
   url_launcher_windows
 )
 
